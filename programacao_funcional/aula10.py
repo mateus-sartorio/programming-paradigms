ebd27d2749a7778a9fe0276e65cc396e561f169b
MateusSartorio <mateussartorio39@gmail.com>
2023-06-08 14:38:35 -0300

    aula 10 concluida

--- programacao_funcional/aula10.py
+++ programacao_funcional/aula10.py
@@ -1,5 +1,14 @@
 # a = set([1, 2, 3, 3])
 # print(a)
 
-a = dict([('jan', 1), ('fev', 2)])
-print(a)
+# a = dict([('jan', 1), ('fev', 2)])
+# print(a)
+
+# print(tuple(iter([1, 2, 3])))
+
+def proximos(it):
+    return [next(it), next(it), next(it)]
+
+# print(proximos(iter([1, 2, 3, 4])))
+
+
